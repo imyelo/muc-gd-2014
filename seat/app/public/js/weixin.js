d5fb97433237b936f96cba45af04db3b3181433c
yelo <zhihuzeye@gmail.com>
2014-12-19 01:51:48 +0800

    update css

--- seat/app/public/js/weixin.js
+++ seat/app/public/js/weixin.js
@@ -1,7 +1,7 @@
 WeixinApi.ready(function (Api) {
   var data = {
     "appId": "",
-    "imgUrl": "http://muc.lab4310.com/public/image/icon.png",
+    "imgUrl": "http://muc2014.lab4310.com/public/image/icon.png",
     "link": window.location.href,
     "desc": "MUC GD 2014",
     "title": "我正在参加 MUC-2014-广东校友会"
